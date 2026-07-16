7650d765b670e27fee530fda7fba0e27b5e01eef
Taj-mim <tajmim915@gmail.com>
2026-07-17 01:17:40 +0600

    solve this question using array and comparing

---
+++ Find-the-Difference.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def findTheDifference(self, s, t):
+        l1=sorted(s)
+        l2=sorted(t)
+        for i in range(0,len(s)):
+            if(l2[i]!=l1[i]):
+                return l2[i]
+        return l2[-1]
+s=input()
+t=input()
+
+sol =Solution()
+print(sol.findTheDifference(s,t))

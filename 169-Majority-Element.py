b9473cf257cf4ceb2e583ecfac3ee1ff05c4d988
Taj-mim <tajmim915@gmail.com>
2026-07-23 00:19:53 +0600

    Add the ans of this question

--- 169-Majority-Element.py
+++ 169-Majority-Element.py
@@ -8,6 +8,12 @@ Example 1:
 Input: nums = [3,3,4]
 Output: 3
 """
+from collections import Counter
 
 num=list(map(int,input().split(',')))
-print(max(num))
+freq=Counter(num)
+maxcount=max(freq.values())
+most_common_values=freq.most_common(1)[0][0]
+#most_common_values = [key for key, val in freq.items() if val == maxcount]  this is for multiple value if occur same timess
+
+print(most_common_values)

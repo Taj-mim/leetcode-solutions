2406d6cccef1c1c09fe7c7c40c02432a1bc0e0de
Taj-mim <tajmim915@gmail.com>
2026-07-22 21:47:12 +0600

    initialize problem statement and basic code

---
+++ 169-Majority-Element.py
@@ -0,0 +1,13 @@
+"""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+Example 1:
+
+Input: nums = [3,3,4]
+Output: 3
+"""
+
+num=list(map(int,input().split(',')))
+print(max(num))

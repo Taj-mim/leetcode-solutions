4dd070c6d8c5707c1247bf82a25035eb7ce8ac2b
Taj-mim <tajmim915@gmail.com>
2026-07-23 00:49:00 +0600

    Solve this Question with sorted() function

---
+++ 242-Valid-Anagram.py
@@ -0,0 +1,23 @@
+"""Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false"""
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        firstword=list(s)
+        secondword=list(t)
+        return sorted(firstword)==sorted(secondword)
+s=input()
+t=input()
+sol=Solution()
+print(sol.isAnagram(s,t))
